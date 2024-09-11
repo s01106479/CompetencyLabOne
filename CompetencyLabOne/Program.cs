7dd4a3c901da2fb41d15c7526f1b8311cb7d5bdd
Isabella Tomasek <s01106479@acad.tri-c.edu>
2024-09-10 21:35:10 -0400

    Add project files.

---
+++ CompetencyLabOne/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompetencyLabOne
+//1. Add a multi-line comment with your name and email address to your Lab One code.
+/* Isabella Tomasek
+ S01106479@acad.tri-c.edu */
+
+//2. Add a single line comment with your course number and CRN to your Lab One code.
+//Course Number: 1050, CRN: 86411
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //3. Add an output statement that prints "This is the first Lab Assignment - Lab One" to the console(screen).
+            Console.WriteLine("This is the first Lab Assignment - Lab One");
+
+            //4. Add an output statement that says, "Hello your Name". If your name is Joe, you would output Hello Joe. You can include your last name if you want.
+            Console.WriteLine("Hello Isabella.");
+
+            //5. Add an output statement that displays your major. Use undeclared if you're not working towards a degree.
+            Console.WriteLine("AA-Computer Science");
+
+            //6. Create an integer variable for the number of times you have written code. Estimate and assign the number of times you have written code in any programming language. Output the value of the variable to the console(screen).
+            int codingFrequency;
+            codingFrequency = 600;
+            Console.WriteLine(codingFrequency);
+
+            //7. Create a string variable for the current semester. Assign the name of the current semester to your string variable. Output the value of the variable to the console(screen).
+            string semester;
+            semester = "fallSemester";
+            Console.WriteLine(semester);
+
+            //8. Create a char variable to store a character. Assign your favorite letter of the alphabet to your char variable. Output the value of the variable to the console(screen).
+            char myFavLetter;
+            myFavLetter = 'V';
+            Console.WriteLine(myFavLetter);
+
+        }
+    }
+}
---
+++ CompetencyLabOne/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CompetencyLabOne")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CompetencyLabOne")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("337cea01-bcdd-4318-9961-509c5e29d677")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
